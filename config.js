49d45938f82dba50d07d0d54dfdfa1f2a7b886e4
shreyasattenapalli <93232171+shreyasattenapalli@users.noreply.github.com>
2023-11-27 07:30:33 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBnG-3cteY2OAxz61QmtzDjk6R8qfVITKY",
+  authDomain: "story-telling-app-c0cf2.firebaseapp.com",
+  databaseURL: "https://story-telling-app-c0cf2-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-c0cf2",
+  storageBucket: "story-telling-app-c0cf2.appspot.com",
+  messagingSenderId: "646007493370",
+  appId: "1:646007493370:web:e3e0e8fedcd9832b226f43"
+};
